d7600f4203688a2fd61f37da8f8434c95e948a92
Shuchen Yan <syan58@hodor01.stat.wisc.edu>
2022-12-01 21:39:04 -0600

    .

--- plot_aris.m
+++ plot_aris.m
@@ -1,4 +1,5 @@
 clc;clear;
-main_dir = 'D:\OneDrive\OneDrive - UW-Madison\Sunduz\SimulationData\settingSSoS\n100-k2-p_in2btw25-p_out1btw1';
+% main_dir = 'D:\OneDrive\OneDrive - UW-Madison\Sunduz\SimulationData\settingSSoS\n100-k2-p_in2btw25-p_out1btw1';
+main_dir = '/storage08/shuchen/SimulationData/settingSSoT/n500-k10-p_in2btw4-p_out1btw1';
 
-plot_all_ari(main_dir, 1, true);
+plot_all_ari(main_dir, 1, false);
